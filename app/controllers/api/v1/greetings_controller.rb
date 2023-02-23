e9d46a05138de12e3349e5144aec05c49aa109b9
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-23 05:38:33 +0000

    Add index and create actions

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
+    @greetings = Greeting.all
+    render json: @greetings
   end
 
-  def show
+  def create
+    @greeting = Greeting.create(greeting_params)
+    if @greeting.save
+    render json: @greeting
+    else
+      render json: {error: 'Error creating greeting'}
+    end
+  end
+
+  def greeting_params
+    params.require(:greeting).permit(:greeting)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :greetings, only: [:index, :show] 
+      resources :greetings, only: [:index, :show, :create] 
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
